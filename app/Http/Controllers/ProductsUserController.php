5c09a42e740d1596e54e962f2eda75531cf6483e
ximenacoronel <ximena.johanna.coronel@gmail.com>
2024-12-15 21:59:11 -0300

    actualizacion nocturna 1

--- app/Http/Controllers/ProductsUserController.php
+++ app/Http/Controllers/ProductsUserController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductsUserController extends Controller
 {
-    public function index(){
-        return view('catalogo');
+    public function index(Request $request){
+        
+        $productos = Product::query();
+
+
+        if ($request->name) {
+            $productos->where('name', 'like', '%' .  $request->name . '%');
+        }
+
+        $request->session()->flashInput($request->all());
+        return view('catalogo', [
+            'productos' => $productos->paginate(5),
+        ]);
         
     }
     public function search(){
@@ -16,4 +28,4 @@ class ProductsUserController extends Controller
     public function show(){
         
     }
-}
+}   
--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -1,146 +1,26 @@
 <x-app-layout>
-<div class="album py-5 bg-light">
+    <div class="album py-5 bg-light">
         <div class="container">
 
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="imagenes/audiculares.jpg" alt="Audiculares">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+            <div class="row">
+                @foreach ($productos as $pro)
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <h3>{{ $pro->name }}</h3>
+                            <img class="card-img-top" data-src="/images/{{ $pro->image }}" alt="Audiculares">
+                            <div class="card-body">
+                                <p class="card-text">{{ $pro->description }}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <small class="text-muted">en Stock</small>
-                  </div>
-                </div>
-              </div>
+                @endforeach
             </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Teclado">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">en Stock</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Mouse">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">Nuevo</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Parlantes">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">en Stock</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Microfono">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">en Stock</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Pc">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">en Oferta</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Monitor">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">en Stock</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Usb">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">en Oferta</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="#" alt="Estabilizador">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Ver</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
-                    </div>
-                    <small class="text-muted">Nuevo</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
 </x-app-layout>
